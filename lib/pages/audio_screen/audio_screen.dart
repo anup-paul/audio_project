577c0be245fdb0897a866edeb2266be2ef8b7325
anup_paul <anuppaul1845@gmail.com>
2022-10-26 00:17:00 +0600

    ui update

--- lib/pages/audio_screen/audio_screen.dart
+++ lib/pages/audio_screen/audio_screen.dart
@@ -40,223 +40,237 @@ class _AudioScreeenState extends State<AudioScreeen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: const Color.fromARGB(255, 227, 227, 227),
         appBar: AppBar(
-          backgroundColor: Colors.white,
+          backgroundColor: const Color.fromARGB(255, 227, 227, 227),
           elevation: 0,
         ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              const Text(
-                'Test 1 of 4',
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.01,
-              ),
-              const Text(
-                'Please press the numbers in this sequence 4 7 3 5 8 and click next',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.04,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                child: GridView.count(
-                  shrinkWrap: true,
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 8.0,
-                  mainAxisSpacing: 8.0,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        initPlayer.stop();
-                        setState(() {
-                          isTrue = false;
-                        });
-                        player.play(AssetSource('00.mp3'));
-                        player.onPlayerComplete.listen((event) {
-                          player.stop();
-                        });
-                      },
-                      child: const DigitWidget(
-                        title: "1",
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        initPlayer.stop();
-                        setState(() {
-                          isTrue = false;
-                        });
-                        player.play(AssetSource('02.mp3'));
-                        player.onPlayerComplete.listen((event) {
-                          player.stop();
-                        });
-                      },
-                      child: const DigitWidget(
-                        title: "2",
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        initPlayer.stop();
-                        setState(() {
-                          isTrue = false;
-                        });
-                        player.play(AssetSource('03.mp3'));
-                        player.onPlayerComplete.listen((event) {
-                          player.stop();
-                        });
-                      },
-                      child: const DigitWidget(
-                        title: "3",
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        initPlayer.stop();
-                        setState(() {
-                          isTrue = false;
-                        });
-                        player.play(AssetSource('04.mp3'));
-                        player.onPlayerComplete.listen((event) {
-                          player.stop();
-                        });
-                      },
-                      child: const DigitWidget(
-                        title: "4",
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        initPlayer.stop();
-                        setState(() {
-                          isTrue = false;
-                        });
-                        player.play(AssetSource('05.mp3'));
-                        player.onPlayerComplete.listen((event) {
-                          player.stop();
-                        });
-                      },
-                      child: const DigitWidget(
-                        title: "5",
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        initPlayer.stop();
-                        setState(() {
-                          isTrue = false;
-                        });
-                        player.play(AssetSource('06.mp3'));
-                        player.onPlayerComplete.listen((event) {
-                          player.stop();
-                        });
-                      },
-                      child: const DigitWidget(
-                        title: "6",
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        initPlayer.stop();
-                        setState(() {
-                          isTrue = false;
-                        });
-                        player.play(AssetSource('07.mp3'));
-                        player.onPlayerComplete.listen((event) {
-                          player.stop();
-                        });
-                      },
-                      child: const DigitWidget(
-                        title: "7",
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        initPlayer.stop();
-                        setState(() {
-                          isTrue = false;
-                        });
-                        player.play(AssetSource('08.mp3'));
-                        player.onPlayerComplete.listen((event) {
-                          player.stop();
-                        });
-                      },
-                      child: const DigitWidget(
-                        title: "8",
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        initPlayer.stop();
-                        setState(() {
-                          isTrue = false;
-                        });
-                        player.play(AssetSource('09.mp3'));
-                        player.onPlayerComplete.listen((event) {
-                          player.stop();
-                        });
-                      },
-                      child: const DigitWidget(
-                        title: "9",
-                      ),
-                    ),
-                    const SizedBox.shrink(),
-                    InkWell(
-                      onTap: () {
-                        initPlayer.stop();
-                        setState(() {
-                          isTrue = false;
-                        });
-                        player.play(AssetSource('00.mp3'));
-                        player.onPlayerComplete.listen((event) {
-                          player.stop();
-                        });
-                      },
-                      child: const DigitWidget(
-                        title: "0",
-                      ),
-                    ),
-                  ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Text(
+                  'Test 1 of 4',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold),
                 ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.04,
-              ),
-              InkWell(
-                onTap: () {},
-                child: Center(
-                    child: Container(
-                  width: MediaQuery.of(context).size.width * 0.3,
-                  padding: const EdgeInsets.all(8),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.01,
+                ),
+                const Text(
+                  'Please press the numbers in this sequence 4 7 3 5 8 and click next',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.04,
+                ),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: Color.fromARGB(255, 180, 114, 85)),
-                  child: const Center(
-                    child: Text(
-                      "Next",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold),
-                    ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const TextField(
+                    cursorColor: Colors.white,
+                    decoration: InputDecoration(border: InputBorder.none),
                   ),
-                )),
-              ),
-            ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: GridView.count(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 5.0,
+                    mainAxisSpacing: 5.0,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          initPlayer.stop();
+                          setState(() {
+                            isTrue = false;
+                          });
+                          player.play(AssetSource('00.mp3'));
+                          player.onPlayerComplete.listen((event) {
+                            player.stop();
+                          });
+                        },
+                        child: const DigitWidget(
+                          title: "1",
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          initPlayer.stop();
+                          setState(() {
+                            isTrue = false;
+                          });
+                          player.play(AssetSource('02.mp3'));
+                          player.onPlayerComplete.listen((event) {
+                            player.stop();
+                          });
+                        },
+                        child: const DigitWidget(
+                          title: "2",
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          initPlayer.stop();
+                          setState(() {
+                            isTrue = false;
+                          });
+                          player.play(AssetSource('03.mp3'));
+                          player.onPlayerComplete.listen((event) {
+                            player.stop();
+                          });
+                        },
+                        child: const DigitWidget(
+                          title: "3",
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          initPlayer.stop();
+                          setState(() {
+                            isTrue = false;
+                          });
+                          player.play(AssetSource('04.mp3'));
+                          player.onPlayerComplete.listen((event) {
+                            player.stop();
+                          });
+                        },
+                        child: const DigitWidget(
+                          title: "4",
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          initPlayer.stop();
+                          setState(() {
+                            isTrue = false;
+                          });
+                          player.play(AssetSource('05.mp3'));
+                          player.onPlayerComplete.listen((event) {
+                            player.stop();
+                          });
+                        },
+                        child: const DigitWidget(
+                          title: "5",
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          initPlayer.stop();
+                          setState(() {
+                            isTrue = false;
+                          });
+                          player.play(AssetSource('06.mp3'));
+                          player.onPlayerComplete.listen((event) {
+                            player.stop();
+                          });
+                        },
+                        child: const DigitWidget(
+                          title: "6",
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          initPlayer.stop();
+                          setState(() {
+                            isTrue = false;
+                          });
+                          player.play(AssetSource('07.mp3'));
+                          player.onPlayerComplete.listen((event) {
+                            player.stop();
+                          });
+                        },
+                        child: const DigitWidget(
+                          title: "7",
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          initPlayer.stop();
+                          setState(() {
+                            isTrue = false;
+                          });
+                          player.play(AssetSource('08.mp3'));
+                          player.onPlayerComplete.listen((event) {
+                            player.stop();
+                          });
+                        },
+                        child: const DigitWidget(
+                          title: "8",
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          initPlayer.stop();
+                          setState(() {
+                            isTrue = false;
+                          });
+                          player.play(AssetSource('09.mp3'));
+                          player.onPlayerComplete.listen((event) {
+                            player.stop();
+                          });
+                        },
+                        child: const DigitWidget(
+                          title: "9",
+                        ),
+                      ),
+                      const SizedBox.shrink(),
+                      InkWell(
+                        onTap: () {
+                          initPlayer.stop();
+                          setState(() {
+                            isTrue = false;
+                          });
+                          player.play(AssetSource('00.mp3'));
+                          player.onPlayerComplete.listen((event) {
+                            player.stop();
+                          });
+                        },
+                        child: const DigitWidget(
+                          title: "0",
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // SizedBox(
+                //   height: MediaQuery.of(context).size.height * 0.04,
+                // ),
+                InkWell(
+                  onTap: () {},
+                  child: Center(
+                      child: Container(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: const Color.fromARGB(255, 180, 114, 85)),
+                    child: const Center(
+                      child: Text(
+                        "Next",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )),
+                ),
+              ],
+            ),
           ),
         ));
   }
@@ -272,12 +286,13 @@ class DigitWidget extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(2),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8), color: Colors.grey),
+        borderRadius: BorderRadius.circular(8),
+      ),
       child: Center(
         child: Text(
           title!,
           style: const TextStyle(
-              color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+              color: Colors.grey, fontSize: 30, fontWeight: FontWeight.bold),
         ),
       ),
     );
